454ecef7cc11e02eca6849cafbd13af61b7dbf92
Nikki A <nikki.apostolakis@gmail.com>
2019-04-01 17:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value)
 {
-    Object.assign({}, driver, {[key]: value})
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
 }
